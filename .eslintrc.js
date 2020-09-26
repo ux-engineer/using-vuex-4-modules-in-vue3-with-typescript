214cc8559d018bf0483a37340ccf43c90294e612
Janne Kurkinen <janne.kurkinen@solita.fi>
2020-09-26 18:54:25 +0300

    Add .env file for setting up NODE_ENV default to 'development', configure ESLint rules for vue3-recommended

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     node: true,
   },
   extends: [
-    'plugin:vue/vue3-essential',
+    'plugin:vue/vue3-recommended',
     '@vue/airbnb',
     '@vue/typescript/recommended',
   ],
@@ -14,5 +14,6 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'import/prefer-default-export': 0,
   },
 };
